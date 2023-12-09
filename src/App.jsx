a8ef8b6d427cf9db5549df505b77145daeb58383
harshitaHD <harshitadhurwe@gmail.com>
2023-12-09 17:39:28 +0530

    Updated Edit Appointment

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,80 @@
+// import "./App.css";
+// import Calendar from "./components/Calendar";
+// import SideNav from "./components/SideNav";
+// import Dashboard from "./components/Dashboard";
+// import { useState, useEffect } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// function App() {
+//   let initAppointment;
+//   if (localStorage.getItem("schedule") === null) {
+//     initAppointment = [];
+//   } else {
+//     initAppointment = JSON.parse(localStorage.getItem("schedule"));
+//   }
+
+//   const onEdit = (updatedAppointment) => {
+//     let sno = oldAppointment.sno;
+//     updatedAppointment.sno = sno;
+//     const newValues = [...values, updatedAppointment];
+//     setValues(newValues);
+//   };
+
+//   const onDelete = (appointment) => {
+//     setValues(
+//       values.filter((e) => {
+//         console.log("deleting");
+//         return e !== appointment;
+//       })
+//     );
+//     localStorage.setItem("schedule", JSON.stringify(appointment));
+//   };
+
+//   const addSchedule = (data) => {
+//     const snoKey = data.hasOwnProperty("sno");
+//     if (!snoKey) {
+//       let sno;
+//       if (values.length === 0) {
+//         sno = 1;
+//       } else {
+//         sno = values[values.length - 1].sno + 1;
+//       }
+//       data.sno = sno;
+//     }
+//     const newValues = [...values, data];
+//     setValues(newValues);
+//   };
+
+//   const [values, setValues] = useState(initAppointment);
+
+//   useEffect(() => {
+//     localStorage.setItem("schedule", JSON.stringify(values));
+//   }, [values]);
+
+//   return (
+//     <>
+//       <SideNav />
+//       <Router>
+//         <Routes>
+//           <Route
+//             exact
+//             path="/"
+//             element={
+//               <Dashboard
+//                 addSchedule={addSchedule}
+//                 values={values}
+//                 onDelete={onDelete}
+//               />
+//             }
+//           ></Route>
+//           <Route exact path="/calendar" element={<Calendar />}></Route>
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
 import "./App.css";
 import Calendar from "./components/Calendar";
 import SideNav from "./components/SideNav";
@@ -13,7 +90,7 @@ function App() {
     initAppointment = JSON.parse(localStorage.getItem("schedule"));
   }
 
-  const onEdit = (updatedAppointment) => {
+  const onEdit = (oldAppointment, updatedAppointment) => {
     let sno = oldAppointment.sno;
     updatedAppointment.sno = sno;
     const newValues = [...values, updatedAppointment];
@@ -23,11 +100,11 @@ function App() {
   const onDelete = (appointment) => {
     setValues(
       values.filter((e) => {
-        console.log("deleting");
+        // console.log("deleting");
         return e !== appointment;
       })
     );
-    localStorage.setItem("schedule", JSON.stringify(appointment));
+    localStorage.setItem("schedule", JSON.stringify(values));
   };
 
   const addSchedule = (data) => {
@@ -64,10 +141,11 @@ function App() {
                 addSchedule={addSchedule}
                 values={values}
                 onDelete={onDelete}
+                onEdit={onEdit}
               />
             }
           ></Route>
-          <Route exact path="/calendar" element={<Calendar />}></Route>
+          <Route exact path="/calendar" element={<Calendar />} />
         </Routes>
       </Router>
     </>
